5152d44c0e93ed3f2069edce142fd04fd128e9f4
Stergios Anastasiadis <Stelioslonx@outlook.com>
2021-06-08 00:00:30 +0200

    make-it-better

--- deploy.py
+++ deploy.py
@@ -4,7 +4,5 @@ print('CommitMsg', end=':')
 commitMsg = input()
 
 os.system('git add .')
-print('-------------------------')
 os.system('git commit -m ' + commitMsg)
-print('-------------------------')
 os.system('git push -u origin master')
